d5630949870ac7aa63e18a13b9c3a98a93ecbe21
DESKTOP-SUPS45P\qqs <lixia@gzqqs.com>
2018-08-22 11:20:16 +0800

    finish city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul class='list'>
+    <li>A</li>
+    <li>A</li>
+    <li>A</li>
+    <li>A</li>
+    <li>A</li>
+    <li>A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'Alphabet',
+  data () {
+    return {
+      name: []
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+  @import '~styles/varibles.styl'
+  .list{
+    position: absolute
+    right 0
+    bottom 0
+    top -.4rem
+    width .4rem
+    display flex
+    flex-direction column // 垂直居中
+    justify-content center
+    li{
+      text-align center
+      line-height .4rem
+      color $bgColor
+    }
+  }
+</style>
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,27 +1,109 @@
 <template>
-  <div class='list'>
-    <div class='area'>
-      <div class='title border-topbottom'>当前城市</div>
-      <div class='button-list'>
-        <div class='button'>北京</div>
-      </div>
-    </div>
-    <div class='area'>
-      <div class='title border-topbottom'>热门城市</div>
-    </div>
-    <div class='area'>
-      <div class='title border-topbottom'>A</div>
-    </div>
+  <div class='list' ref='wrapper'>
+     <div>
+       <div class='area'>
+         <div class='title border-topbottom'>当前城市</div>
+         <div class='button-list'>
+           <div class='button-wrapper'>
+             <div class='button'>北京</div>
+           </div>
+         </div>
+       </div>
+       <div class='area'>
+         <div class='title border-topbottom'>热门城市</div>
+         <div class='button-list'>
+           <div class='button-wrapper'>
+             <div class='button'>北京</div>
+           </div>
+           <div class='button-wrapper'>
+             <div class='button'>北京</div>
+           </div>
+           <div class='button-wrapper'>
+             <div class='button'>北京</div>
+           </div>
+           <div class='button-wrapper'>
+             <div class='button'>北京</div>
+           </div>
+           <div class='button-wrapper'>
+             <div class='button'>北京</div>
+           </div>
+           <div class='button-wrapper'>
+             <div class='button'>北京</div>
+           </div>
+           <div class='button-wrapper'>
+             <div class='button'>北京</div>
+           </div>
+         </div>
+       </div>
+       <div class='area'>
+         <div class='title border-topbottom'>A</div>
+         <ul class='item-list'>
+           <li class='border-bottom'>阿拉尔</li>
+           <li class='border-bottom'>阿拉尔</li>
+           <li class='border-bottom'>阿拉尔</li>
+           <li class='border-bottom'>阿拉尔</li>
+         </ul>
+       </div>
+       <div class='area'>
+         <div class='title border-topbottom'>B</div>
+         <ul class='item-list'>
+           <li class='border-bottom'>阿拉尔</li>
+           <li class='border-bottom'>阿拉尔</li>
+           <li class='border-bottom'>阿拉尔</li>
+           <li class='border-bottom'>阿拉尔</li>
+         </ul>
+       </div>
+       <div class='area'>
+         <div class='title border-topbottom'>C</div>
+         <ul class='item-list'>
+           <li class='border-bottom'>阿拉尔</li>
+           <li class='border-bottom'>阿拉尔</li>
+           <li class='border-bottom'>阿拉尔</li>
+           <li class='border-bottom'>阿拉尔</li>
+         </ul>
+       </div>
+       <div class='area'>
+         <div class='title border-topbottom'>A</div>
+         <ul class='item-list'>
+           <li class='border-bottom'>阿拉尔</li>
+           <li class='border-bottom'>阿拉尔</li>
+           <li class='border-bottom'>阿拉尔</li>
+           <li class='border-bottom'>阿拉尔</li>
+         </ul>
+       </div>
+       <div class='area'>
+         <div class='title border-topbottom'>B</div>
+         <ul class='item-list'>
+           <li class='border-bottom'>阿拉尔</li>
+           <li class='border-bottom'>阿拉尔</li>
+           <li class='border-bottom'>阿拉尔</li>
+           <li class='border-bottom'>阿拉尔</li>
+         </ul>
+       </div>
+       <div class='area'>
+         <div class='title border-topbottom'>C</div>
+         <ul class='item-list'>
+           <li class='border-bottom'>阿拉尔</li>
+           <li class='border-bottom'>阿拉尔</li>
+           <li class='border-bottom'>阿拉尔</li>
+           <li class='border-bottom'>阿拉尔</li>
+         </ul>
+       </div>
+     </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
   name: 'CityList',
   data () {
     return {
       name: []
     }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.wrapper)
   }
 }
 </script>
@@ -36,11 +118,45 @@ export default {
       border-color #ccc
     }
   }
-  .title{
-    line-height: .44rem
-    background-color #eee
-    padding-left: .2rem
-    color #666
-    font-size .26rem
+  .border-bottom{
+    &:before{
+      border-color #ccc
+    }
+  }
+  .list{
+    position: absolute
+    top: 1.55rem;
+    left: 0
+    bottom: 0
+    right: 0
+    overflow hidden
+    .title{
+      line-height: .54rem
+      background-color #eee
+      padding-left: .2rem
+      color #666
+      font-size .26rem
+    }
+    .button-list{
+      padding: .1rem .6rem  .1rem .1rem
+      display flex
+      flex-wrap wrap
+      .button-wrapper{
+        width: 33.33%
+        .button{
+          text-align center
+          border:.02rem solid #ccc
+          margin .1rem
+          padding: .1rem 0
+          border-radius .06rem
+        }
+      }
+    }
+    .item-list{
+      li{
+        line-height: .70rem
+        padding-left .2rem
+      }
+    }
   }
 </style>
