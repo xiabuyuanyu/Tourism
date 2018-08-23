3c1ddf887a7d302d16c9630e18b422f49d87cf3c
DESKTOP-SUPS45P\qqs <lixia@gzqqs.com>
2018-08-23 17:41:41 +0800

    change city

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -3,16 +3,16 @@
     <div class="search">
       <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
     </div>
-    <!--<div class="search-content" ref="search" v-show="keyword">-->
-      <!--<ul>-->
-        <!--<li class="search-item border-bottom" v-for="item of list" :key="item.id" @click="handleCityClick(item.name)">-->
-          <!--{{item.name}}-->
-        <!--</li>-->
-        <!--<li class="search-item border-bottom" v-show="hasNoData">-->
-          <!--没有找到匹配数据-->
-        <!--</li>-->
-      <!--</ul>-->
-    <!--</div>-->
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id" @click="handleCityClick(item.name)">
+          {{item.name}}
+        </li>
+        <li class="search-item border-bottom" v-show="hasNoData">
+          没有找到匹配数据
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -22,7 +22,13 @@ export default {
   data () {
     return {
       keyword: '',
-      hasNoData: true
+      hasNoData: true,
+      list: []
+    }
+  },
+  methods: {
+    handleCityClick (red) {
+
     }
   }
 }
