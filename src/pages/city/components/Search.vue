cde14d738b6eb9a947c8b62b425a4429b957be09
DESKTOP-SUPS45P\qqs <lixia@gzqqs.com>
2018-08-24 11:15:48 +0800

    search list

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -17,14 +17,48 @@
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
   name: 'CitySearch',
+  props: {
+    cities: Object
+  },
   data () {
     return {
       keyword: '',
-      hasNoData: true,
-      list: []
+      list: [],
+      timer: null
     }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.search)
   },
   methods: {
     handleCityClick (red) {
