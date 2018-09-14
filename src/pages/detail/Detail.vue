c4e21eaa2b327da2a6d3a2f03b4065771d930ef3
DESKTOP-SUPS45P\qqs <lixia@gzqqs.com>
2018-09-14 17:28:43 +0800

    detailList

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,43 +1,64 @@
 <template>
-    <div>
-      <detail-banner></detail-banner>
-      <detail-header></detail-header>
-      <div class="content">
-      </div>
+  <div>
+    <detail-banner
+      :sightName="sightName"
+      :bannerImg="bannerImg"
+      :bannerImgs="gallaryImgs"
+    ></detail-banner>
+    <detail-header></detail-header>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
     </div>
+  </div>
 </template>
 
 <script>
-import DetailBanner from './components/banner'
-import DetailHeader from './components/header'
+import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
+import DetailList from './components/List'
+import axios from 'axios'
 export default {
-  name: 'City',
-  data () {
-    return {
-      cities: {},
-      hotCities: [],
-      letter: ''
-    }
-  },
+  name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
   },
-  mounted () {
+  data () {
+    return {
+      sightName: '',
+      bannerImg: '',
+      gallaryImgs: [],
+      list: []
+    }
   },
   methods: {
-    getCityInfo () {
+    getDetailInfo () {
+      axios.get('/api/detail.json', {
+        params: {
+          id: this.$route.params.id
+        }
+      }).then(this.handleGetDataSucc)
     },
-    hendldGetCityinfoSucc (res) {
-    },
-    handletterChange (letter) {
+    handleGetDataSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.sightName = data.sightName
+        this.bannerImg = data.bannerImg
+        this.gallaryImgs = data.gallaryImgs
+        this.list = data.categoryList
+      }
     }
+  },
+  mounted () {
+    this.getDetailInfo()
   }
 }
 </script>
 
-<style lang='stylus' scoped>
-.content{
-  height: 50rem
-}
+<style lang="stylus" scoped>
+  .content{
+    height: 50rem
+  }
 </style>
